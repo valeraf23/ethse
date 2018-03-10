5ca36c332f0a8a3c91845eebe30973c52992e01a
valeraf23 <valeraf23@gmail.com>
2018-03-10 16:23:31 +0200

    done with other tests

--- test/debts.js
+++ test/debts.js
@@ -56,11 +56,32 @@ contract('Debts', function(accounts) {
     .then(asserts.equal(value));
   });
 
-  it('should emit Repayed event on repay');
+  it('should emit Repayed event on repay', () => {
+    const borrower = accounts[3];
+    const value = 1000;
+    return Promise.resolve()
+    .then(() => debts.borrow(value, {from: borrower}))
+    .then(() => debts.repay(borrower, value, {from: OWNER}))
+    .then(result => {
+      assert.equal(result.logs.length, 1);
+      assert.equal(result.logs[0].event, 'Repayed');
+      assert.equal(result.logs[0].args.by, borrower);
+      assert.equal(result.logs[0].args.value.valueOf(), value);
+    });
+  });
 
-  it('should not allow owner to borrow');
+  it('should not allow owner to borrow', () => {
+    const value = 1000;
+    return Promise.resolve()
+    .then(() => debts.borrow.call(value, {from: OWNER}))
+    .then(asserts.equal(false));
+  });
 
-  it('should not allow not owner to repay');
-
-  it('should direct you for inventing more tests');
+  it('should not allow not owner to repay', () => {
+    const borrower = accounts[3];
+    const value = 1000;
+    return Promise.resolve()
+    .then(() => debts.repay.call(borrower, value, {from: borrower}))
+    .then(asserts.equal(false));
+  });
 });
