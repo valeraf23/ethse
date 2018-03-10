0911eb74ff4264a540a41db63ddc96740e2d6fd2
valeraf23 <valeraf23@gmail.com>
2018-03-10 17:49:47 +0200

    constant8

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -3,7 +3,5 @@ const OffChain = artifacts.require('./LendOrder.sol');
 
 module.exports = deployer => {
   deployer.deploy(Debts);
-};
-module.exports = deployer => {
   deployer.deploy(OffChain);
 };
