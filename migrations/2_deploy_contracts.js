1aea7c16c7df5f3699ed497b8ac3a4d4ff4210c8
valeraf23 <valeraf23@gmail.com>
2018-03-25 01:29:58 +0200

    oxoFactory tests

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,7 +1,11 @@
 const Debts = artifacts.require('./Debts.sol');
 const OffChain = artifacts.require('./LendOrder.sol');
+const oxoFactory = artifacts.require('./OxoFactory.sol');
+const oxo = artifacts.require('./OXO.sol');
 
 module.exports = deployer => {
   deployer.deploy(Debts);
   deployer.deploy(OffChain);
+  deployer.deploy(oxoFactory);
+  deployer.deploy(oxo);
 };
